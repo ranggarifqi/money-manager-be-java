ebc25a89d44ed5d42bdc0ff7b2376a6b7eb707a3
Rangga Rifqi <rangga@ranggarifqi.com>
2023-04-08 15:04:46 +0700

    Added TransactionCategory entity

---
+++ src/main/java/com/ranggarifqi/moneymanager/entity/TransactionCategory.java
@@ -0,0 +1,54 @@
+package com.ranggarifqi.moneymanager.entity;
+
+import jakarta.persistence.*;
+
+import java.sql.Timestamp;
+import java.util.UUID;
+
+@Entity
+@Table(name = "TransactionCategories")
+public class TransactionCategory extends Audit{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private UUID id;
+
+    @Column(name = "transactionId", nullable = false)
+    private UUID transactionId;
+
+    @Column(name = "categoryId", nullable = false)
+    private UUID categoryId;
+
+    public TransactionCategory() {
+    }
+
+    public TransactionCategory(UUID transactionId, UUID categoryId, Timestamp createdAt, Timestamp updatedAt) {
+        super(createdAt, updatedAt);
+        this.transactionId = transactionId;
+        this.categoryId = categoryId;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public UUID getTransactionId() {
+        return transactionId;
+    }
+
+    public void setTransactionId(UUID transactionId) {
+        this.transactionId = transactionId;
+    }
+
+    public UUID getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(UUID categoryId) {
+        this.categoryId = categoryId;
+    }
+}
