5c3d59b6b33a374bcac059a0a91c44c29ee2bec4
Rangga Rifqi <rangga@ranggarifqi.com>
2023-04-08 10:04:40 +0700

    First commit

---
+++ src/test/java/com/ranggarifqi/moneymanager/MoneymanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ranggarifqi.moneymanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneymanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
