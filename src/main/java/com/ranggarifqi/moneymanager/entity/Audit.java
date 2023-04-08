3d378707a3313b15149b7287afb190ac552e32fb
Rangga Rifqi <rangga@ranggarifqi.com>
2023-04-08 11:47:16 +0700

    Added constructor, getter, and setter

--- src/main/java/com/ranggarifqi/moneymanager/entity/Audit.java
+++ src/main/java/com/ranggarifqi/moneymanager/entity/Audit.java
@@ -14,4 +14,28 @@ public class Audit {
     @Column(name = "updatedAt", nullable = true)
     @LastModifiedDate
     private Date updatedAt;
+
+    public Audit() {
+    }
+
+    public Audit(Date createdAt, Date updatedAt) {
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
 }
--- src/main/java/com/ranggarifqi/moneymanager/entity/User.java
+++ src/main/java/com/ranggarifqi/moneymanager/entity/User.java
@@ -27,4 +27,57 @@ public class User extends Audit {
 
     @Column(name = "password", nullable = false)
     private String password;
+
+    public User() {
+        super();
+    }
+
+    public User(UUID id, String name, String email, String phone, String password, Date createdAt, Date updatedAt) {
+        super(createdAt, updatedAt);
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+        this.password = password;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
