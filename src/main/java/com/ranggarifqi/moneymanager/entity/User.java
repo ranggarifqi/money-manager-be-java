72e063db2c1df8053944b5b26825c7f4bed82637
Rangga Rifqi <rangga@ranggarifqi.com>
2023-04-08 11:40:53 +0700

    Added User Model

---
+++ src/main/java/com/ranggarifqi/moneymanager/entity/User.java
@@ -0,0 +1,38 @@
+package com.ranggarifqi.moneymanager.entity;
+
+import jakarta.persistence.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.util.Date;
+import java.util.UUID;
+
+@Entity
+@Table(name = "Users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private UUID id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @Column(name = "phone", nullable = false)
+    private String phone;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "createdAt", nullable = false)
+    @CreatedDate
+    private Date createdAt;
+
+    @Column(name = "updatedAt", nullable = true)
+    @LastModifiedDate
+    private Date updatedAt;
+}
