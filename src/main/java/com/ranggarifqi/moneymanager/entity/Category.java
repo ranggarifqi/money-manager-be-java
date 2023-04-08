11665752625ab877f1790ecfec6c3518ad49645b
Rangga Rifqi <rangga@ranggarifqi.com>
2023-04-08 14:41:38 +0700

    Added Category Entity

---
+++ src/main/java/com/ranggarifqi/moneymanager/entity/Category.java
@@ -0,0 +1,55 @@
+package com.ranggarifqi.moneymanager.entity;
+
+import jakarta.persistence.*;
+
+import java.util.Date;
+import java.util.UUID;
+
+@Entity
+@Table(name = "Categories")
+public class Category extends Audit {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private UUID id;
+
+    @Column(name = "parentId", nullable = true)
+    private UUID parentId;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    public Category() {
+    }
+
+    public Category(UUID parentId, String name, Date createdAt, Date updatedAt) {
+        super(createdAt, updatedAt);
+        this.parentId = parentId;
+        this.name = name;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public UUID getParentId() {
+        return parentId;
+    }
+
+    public void setParentId(UUID parentId) {
+        this.parentId = parentId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
