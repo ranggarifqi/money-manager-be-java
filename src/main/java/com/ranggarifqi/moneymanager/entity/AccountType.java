e7e0938b9031d5e4c578cff869e6d8038b6a5258
Rangga Rifqi <rangga@ranggarifqi.com>
2023-04-08 11:50:11 +0700

    Added AccountType Entity

---
+++ src/main/java/com/ranggarifqi/moneymanager/entity/AccountType.java
@@ -0,0 +1,44 @@
+package com.ranggarifqi.moneymanager.entity;
+
+import jakarta.persistence.*;
+
+import java.util.Date;
+import java.util.UUID;
+
+@Entity
+@Table(name = "AccountTypes")
+public class AccountType extends Audit {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private UUID id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    public AccountType() {
+    }
+
+    public AccountType(UUID id, String name, Date createdAt, Date updatedAt) {
+        super(createdAt, updatedAt);
+        this.id = id;
+        this.name = name;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
