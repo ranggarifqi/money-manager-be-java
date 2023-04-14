e5cc1f94ec8dcfa6fcaac65ae7012cbeac15d499
Rangga Rifqi <rangga@ranggarifqi.com>
2023-04-14 07:27:38 +0700

    Implement IUserRepository

---
+++ src/main/java/com/ranggarifqi/moneymanager/user/UserRepository.java
@@ -0,0 +1,40 @@
+package com.ranggarifqi.moneymanager.user;
+
+import com.ranggarifqi.moneymanager.common.exception.NotFoundException;
+import com.ranggarifqi.moneymanager.model.User;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class UserRepository implements IUserRepository {
+
+    private final EntityManager entityManager;
+
+    @Autowired
+    public UserRepository(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    @Override
+    public User findByEmail(String email) {
+        TypedQuery<User> query = this.entityManager.createQuery("FROM User WHERE \"email\" = :email", User.class);
+        query.setParameter("email", email);
+
+        List<User> users = query.getResultList();
+
+        if (users.size() == 0) {
+            throw new NotFoundException("User with email " + email + " not found");
+        }
+
+        return users.get(0);
+    }
+
+    @Override
+    public void create(User payload) {
+        this.entityManager.persist(payload);
+    }
+}
