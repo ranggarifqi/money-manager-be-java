1a3028cc5185f96f2a549db42f36354e87ce19fb
Rangga Rifqi <rangga@ranggarifqi.com>
2023-04-30 15:02:45 +0700

    Updated Security settings on error message

--- src/main/java/com/ranggarifqi/moneymanager/configuration/SecurityConfiguration.java
+++ src/main/java/com/ranggarifqi/moneymanager/configuration/SecurityConfiguration.java
@@ -2,7 +2,6 @@ package com.ranggarifqi.moneymanager.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -13,12 +12,12 @@ public class SecurityConfiguration {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf().ignoringRequestMatchers("/v1/users/signup");
+        http.csrf().disable();
 
         http.authorizeHttpRequests(
             config ->
                 config
-                    .requestMatchers(HttpMethod.POST, "/v1/users/signup").permitAll()
+                    .requestMatchers("/error", "/v1/users/signup").permitAll()
         );
 
         return http.build();
