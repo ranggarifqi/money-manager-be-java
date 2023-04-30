5c08386b1ec176935ef96934864f67e2ca91ee6a
Rangga Rifqi <rangga@ranggarifqi.com>
2023-04-30 14:20:44 +0700

    Added v1 UserController

---
+++ src/main/java/com/ranggarifqi/moneymanager/controllers/v1/UserController.java
@@ -0,0 +1,46 @@
+package com.ranggarifqi.moneymanager.controllers.v1;
+
+import com.ranggarifqi.moneymanager.model.User;
+import com.ranggarifqi.moneymanager.user.IUserService;
+import com.ranggarifqi.moneymanager.user.dto.SignUpDTO;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping(value = "/v1/users")
+public class UserController {
+
+    private final IUserService userService;
+
+    @Autowired
+    public UserController(IUserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping(value = "/signup")
+    public User signUp(@Valid @RequestBody SignUpDTO payload) {
+        return this.userService.signUp(payload);
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, String> handleValidationExceptions(
+            MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+
+        return errors;
+    }
+
+}
