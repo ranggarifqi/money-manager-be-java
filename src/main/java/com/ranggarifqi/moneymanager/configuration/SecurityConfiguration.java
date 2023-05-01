92ea9cb31951f87aa04c5e561d27f104728406bc
Rangga Rifqi <rangga@ranggarifqi.com>
2023-05-01 20:32:05 +0700

    Updated SecurityConfiguration

--- src/main/java/com/ranggarifqi/moneymanager/configuration/SecurityConfiguration.java
+++ src/main/java/com/ranggarifqi/moneymanager/configuration/SecurityConfiguration.java
@@ -1,14 +1,30 @@
 package com.ranggarifqi.moneymanager.configuration;
 
+import com.ranggarifqi.moneymanager.configuration.filter.JWTAuthenticationFilter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfiguration {
+
+    private final JWTAuthenticationFilter jwtAuthFilter;
+
+    @Autowired
+    public SecurityConfiguration(JWTAuthenticationFilter jwtAuthFilter) {
+        this.jwtAuthFilter = jwtAuthFilter;
+    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -22,9 +38,15 @@ public class SecurityConfiguration {
                         "/v1/users/signup",
                         "/v1/users/verify",
                         "/v1/users/signin"
-                    ).permitAll()
+                    )
+                        .permitAll()
+                    .anyRequest()
+                        .authenticated()
         );
 
+        http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        http.addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
 
