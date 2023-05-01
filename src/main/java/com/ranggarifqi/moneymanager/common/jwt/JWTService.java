9bd31afee37496ed7e6d8ba50864ef6f5f598802
Rangga Rifqi <rangga@ranggarifqi.com>
2023-05-01 20:31:47 +0700

    Updated JWTService

--- src/main/java/com/ranggarifqi/moneymanager/common/jwt/JWTService.java
+++ src/main/java/com/ranggarifqi/moneymanager/common/jwt/JWTService.java
@@ -1,13 +1,18 @@
 package com.ranggarifqi.moneymanager.common.jwt;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import java.security.Key;
 import java.time.Clock;
 import java.util.Date;
+import java.util.function.Function;
 
 @Service
 public class JWTService implements IJWTService{
@@ -38,7 +43,39 @@ public class JWTService implements IJWTService{
                 .claim("email", email)
                 .setIssuedAt(now)
                 .setExpiration(new Date(now.getTime() + this.expirationMs))
-                .signWith(SignatureAlgorithm.HS512, this.secret)
+                .signWith(this.getSignInKey())
                 .compact();
     }
+
+    private Key getSignInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(this.secret);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getSignInKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    public String extractEmail(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    public boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    private Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
+
 }
