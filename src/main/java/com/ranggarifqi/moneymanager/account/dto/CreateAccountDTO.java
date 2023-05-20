172db9b1c4c80bfe098d99de5af1774bda9ed3d9
Rangga Rifqi <rangga@ranggarifqi.com>
2023-05-20 17:08:53 +0700

    Added constructor & other boilerplates on CreateAccountDTO

--- src/main/java/com/ranggarifqi/moneymanager/account/dto/CreateAccountDTO.java
+++ src/main/java/com/ranggarifqi/moneymanager/account/dto/CreateAccountDTO.java
@@ -5,16 +5,55 @@ import jakarta.validation.constraints.Positive;
 
 public class CreateAccountDTO {
 
-  @NotBlank(message = "accountType is mandatory")
-  private String accountType;
+  @NotBlank(message = "name is mandatory")
+  private String name;
 
   @NotBlank(message = "userId is mandatory")
   private String userId;
 
-  @NotBlank(message = "name is mandatory")
-  private String name;
+  @NotBlank(message = "accountType is mandatory")
+  private String accountType;
 
   @NotBlank(message = "balance is mandatory")
   @Positive(message = "balance must be positive")
   private double balance;
+
+  public CreateAccountDTO(String name, String userId, String accountType, double balance) {
+    this.name = name;
+    this.userId = userId;
+    this.accountType = accountType;
+    this.balance = balance;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getUserId() {
+    return userId;
+  }
+
+  public void setUserId(String userId) {
+    this.userId = userId;
+  }
+
+  public String getAccountType() {
+    return accountType;
+  }
+
+  public void setAccountType(String accountType) {
+    this.accountType = accountType;
+  }
+
+  public double getBalance() {
+    return balance;
+  }
+
+  public void setBalance(double balance) {
+    this.balance = balance;
+  }
 }
