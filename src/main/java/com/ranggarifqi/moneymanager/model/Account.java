b2fec1531102665ddeabb8394fdab7ea80ddf99b
Rangga Rifqi <rangga@ranggarifqi.com>
2023-05-20 20:05:52 +0700

    Updated PK Generation strategy to AUTO

--- src/main/java/com/ranggarifqi/moneymanager/model/Account.java
+++ src/main/java/com/ranggarifqi/moneymanager/model/Account.java
@@ -10,7 +10,7 @@ import java.util.UUID;
 public class Account extends Audit {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "`id`", nullable = false)
     private UUID id;
 
