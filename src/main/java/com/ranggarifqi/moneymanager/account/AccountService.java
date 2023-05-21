6a5b82fe0dd748297d9f009760de1fe7ac1586bd
Rangga Rifqi <rangga@ranggarifqi.com>
2023-05-21 07:57:57 +0700

    Added IAccountService.findById & findByUserId

--- src/main/java/com/ranggarifqi/moneymanager/account/AccountService.java
+++ src/main/java/com/ranggarifqi/moneymanager/account/AccountService.java
@@ -1,6 +1,7 @@
 package com.ranggarifqi.moneymanager.account;
 
 import com.ranggarifqi.moneymanager.account.dto.CreateAccountDTO;
+import com.ranggarifqi.moneymanager.common.exception.NotFoundException;
 import com.ranggarifqi.moneymanager.model.Account;
 import com.ranggarifqi.moneymanager.model.User;
 import com.ranggarifqi.moneymanager.user.IUserRepository;
@@ -9,6 +10,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.UUID;
 
 @Service
@@ -24,6 +26,22 @@ public class AccountService implements IAccountService{
   public AccountService(IUserRepository userRepository, IAccountRepository accountRepository) {
     this.userRepository = userRepository;
     this.accountRepository = accountRepository;
+  }
+
+  @Override
+  public List<Account> findByUserId(UUID userId) {
+    return this.accountRepository.findByUserId(userId);
+  }
+
+  @Override
+  public Account findById(String id) throws NotFoundException {
+    Account account = this.accountRepository.findById(UUID.fromString(id));
+
+    if (account == null) {
+      throw new NotFoundException("Account with id " + id + " not found");
+    }
+
+    return account;
   }
 
   @Override
--- src/main/java/com/ranggarifqi/moneymanager/account/IAccountService.java
+++ src/main/java/com/ranggarifqi/moneymanager/account/IAccountService.java
@@ -1,8 +1,17 @@
 package com.ranggarifqi.moneymanager.account;
 
 import com.ranggarifqi.moneymanager.account.dto.CreateAccountDTO;
+import com.ranggarifqi.moneymanager.common.exception.NotFoundException;
 import com.ranggarifqi.moneymanager.model.Account;
 
+import java.util.List;
+import java.util.UUID;
+
 public interface IAccountService {
+
+  List<Account> findByUserId(UUID userId);
+
+  Account findById(String id) throws NotFoundException;
+
   Account create(CreateAccountDTO payload, String userId);
 }
