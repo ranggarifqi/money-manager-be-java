e9397c911a2a607653a0b1a107e7da8944b361ea
Rangga Rifqi <rangga@ranggarifqi.com>
2023-05-21 07:51:46 +0700

    Added IAccountRepository.findById & findByUserId

--- src/main/java/com/ranggarifqi/moneymanager/account/AccountRepository.java
+++ src/main/java/com/ranggarifqi/moneymanager/account/AccountRepository.java
@@ -2,11 +2,15 @@ package com.ranggarifqi.moneymanager.account;
 
 import com.ranggarifqi.moneymanager.model.Account;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
 import jakarta.transaction.Transactional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.UUID;
 
 @Repository
 public class AccountRepository implements IAccountRepository{
@@ -18,6 +22,35 @@ public class AccountRepository implements IAccountRepository{
   @Autowired
   public AccountRepository(EntityManager entityManager) {
     this.entityManager = entityManager;
+  }
+
+  @Override
+  public List<Account> findByUserId(UUID userId) {
+    this.logger.info("Finding accounts by user Id " + userId.toString());
+
+    String sql = "FROM Account where userId = :userId";
+
+    TypedQuery<Account> query = this.entityManager.createQuery(sql, Account.class);
+    query.setParameter("userId", userId);
+
+    return query.getResultList();
+  }
+
+  @Override
+  public Account findById(UUID id) {
+    this.logger.info("Finding accounts by account Id " + id.toString());
+
+    String sql = "FROM Account where id = :id";
+    TypedQuery<Account> query = this.entityManager.createQuery(sql, Account.class);
+    query.setParameter("id", id);
+
+    List<Account> result = query.getResultList();
+
+    if (result.size() == 0) {
+      return null;
+    }
+
+    return result.get(0);
   }
 
   @Transactional
--- src/main/java/com/ranggarifqi/moneymanager/account/IAccountRepository.java
+++ src/main/java/com/ranggarifqi/moneymanager/account/IAccountRepository.java
@@ -2,6 +2,13 @@ package com.ranggarifqi.moneymanager.account;
 
 import com.ranggarifqi.moneymanager.model.Account;
 
+import java.util.List;
+import java.util.UUID;
+
 public interface IAccountRepository {
+  List<Account> findByUserId(UUID userId);
+
+  Account findById(UUID id);
+
   void create(Account payload);
 }
