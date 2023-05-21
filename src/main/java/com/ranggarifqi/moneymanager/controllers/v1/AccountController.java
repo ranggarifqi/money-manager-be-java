b77871dec3c25d0465f47be0bcce77d01a96ea5c
Rangga Rifqi <rangga@ranggarifqi.com>
2023-05-21 13:18:35 +0700

    Added AccountController.fetchByOwnerId

--- src/main/java/com/ranggarifqi/moneymanager/controllers/v1/AccountController.java
+++ src/main/java/com/ranggarifqi/moneymanager/controllers/v1/AccountController.java
@@ -17,6 +17,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/v1/accounts")
@@ -29,6 +30,34 @@ public class AccountController {
   @Autowired
   public AccountController(IAccountService accountService) {
     this.accountService = accountService;
+  }
+
+  @GetMapping(value = "")
+  @ResponseBody
+  public ResponseEntity<APIResponse<List<AccountResponse>>> findByOwnerId(Authentication authentication) {
+    User user = this.getAuthenticatedUser(authentication);
+
+    this.logger.info("Find Accounts By owner id: " + user.getId().toString());
+
+    try {
+      List<Account> result = this.accountService.findByUserId(user.getId());
+
+      List<AccountResponse> response = result.stream().map(
+              account -> new AccountResponse(
+                      account.getId().toString(),
+                      account.getUser().getId().toString(),
+                      account.getAccountType(),
+                      account.getName(),
+                      account.getBalance(),
+                      account.getCreatedAt(),
+                      account.getUpdatedAt()
+              )
+      ).toList();
+
+      return APIResponse.constructResponse(response);
+    } catch (Exception e) {
+      throw ErrorResponse.construct(e);
+    }
   }
 
   @GetMapping(value = "/{id}")
@@ -49,8 +78,8 @@ public class AccountController {
 
   @PostMapping(value = "")
   public ResponseEntity<APIResponse<CreateAccountResponse>> create(@Valid @RequestBody CreateAccountDTO body, Authentication authentication) {
+    this.logger.info("Creating a new account");
     try {
-      this.logger.info("Creating a new account");
 
       User user = this.getAuthenticatedUser(authentication);
 
